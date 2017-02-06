7e63ff953d3bcd4e7cde377969292216b6455c97
David Bagdasaryan <david.bagdasaryan@gft.com>
2017-02-06 13:07:00 +0100

    adding .travis.yml file

--- src/main/java/gft/ddba/calendar/model/Calendar.java
+++ src/main/java/gft/ddba/calendar/model/Calendar.java
@@ -1,9 +1,12 @@
 package gft.ddba.calendar.model;
 
+import java.time.LocalDate;
+import java.util.Iterator;
+
 import lombok.Data;
 
 @Data
-public class Calendar {
-
+public class Calendar implements Iterable<LocalDate> {
+	
 	
 }
